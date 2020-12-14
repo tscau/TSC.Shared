2d1c1157e090669b1e311cab7b3bb10275b05da1
Richard Angus <richard.angus@live.com.au>
2020-12-14 10:57:16 +1000

    Added shared code Added nuget pack and push to GitHub packages

---
+++ source/TSC.Shared/CommonMessageFormat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TSC.Shared
+{
+    public class CommonMessageFormat
+    {
+
+        public CommonMessageFormat()
+        {
+            ScheduleID = Guid.NewGuid();
+            JSScheduleId = Guid.NewGuid();
+            BatchId = 0;
+            JobPath = string.Empty;
+            TemplateId = string.Empty;
+            StatusToProcess = 0;
+            IsConsolidationJob = false;
+            Flags = new Flags();
+        }
+
+        public Guid ScheduleID { get; set; }
+        public Guid JSScheduleId { get; set; }
+        public long BatchId { get; set; }
+        public string JobPath { get; set; }
+        public string TemplateId { get; set; }
+        public int StatusToProcess { get; set; }
+        public bool IsConsolidationJob { get; set; }
+
+        public Flags Flags { get; set; }
+    }
+}
---
+++ source/TSC.Shared/Flags.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TSC.Shared
+{
+    public class Flags
+    {
+        public Flags()
+        {
+            AllowDataRetrieve = true;
+            AllowDataUpdate = true;
+            AllowSendEmail = true;
+            AllowSendOther = true;
+            AllowSendPaper = true;
+            AllowSendSMS = true;
+            AllowPdfConsolidation = true;
+            AllowPSCOnsolidation = true;
+        }
+        public bool? AllowDataRetrieve { get; set; }
+        public bool? AllowDataUpdate { get; set; }
+        public bool? AllowSendEmail { get; set; }
+        public bool? AllowSendSMS { get; set; }
+        public bool? AllowSendPaper { get; set; }
+        public bool? AllowSendOther { get; set; }
+        public bool? AllowPSCOnsolidation { get; set; }
+        public bool? AllowPdfConsolidation { get; set; }
+    }
+}
