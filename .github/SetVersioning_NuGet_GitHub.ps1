054da81d899d5cab9db6cb47f828d764caa2dd96
Richard Angus <48549112+rcangus@users.noreply.github.com>
2020-12-15 15:24:39 +1000

    Added versioning PS script

---
+++ .github/SetVersioning_NuGet_GitHub.ps1
@@ -0,0 +1,16 @@
+$rev = $env:GITHUB_RUN_NUMBER
+Write-Host "rev number is " $rev
+
+$branch = $env:GITHUB_REF;
+
+if($branch -like '*feature*'){
+    #get the branch name from the last / 
+    $featureName = $branch.Substring($branch.LastIndexOf('/')+1)
+    $ver = -join("1.0.", $featureName, ".", $rev)
+} else {
+    $ver = -join("1.0.", $rev)
+}
+echo "NUGET_VERSION=$($ver)" >> $GITHUB_ENV
+#Lets try setting an output parameter
+Write-Host "ver = $($ver)"
+echo "::set-output name=NUGET_VERSION::$($ver)"
